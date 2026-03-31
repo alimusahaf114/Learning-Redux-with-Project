7281ea89b6b42c5acc651793f4c21e7f45141447
Musahaf Ali <musahafali43@gmail.com>
2026-03-31 14:00:06 +0500

    created folder structure and added API keys

---
+++ Redux-RealProject/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-white h-screen w-full bg-gray-950'>
+      <h1 className='text-2xl'>Musahaf Bhai Kese Ho Aap</h1>
+    </div>
+  );
+}
+
+export default App;
