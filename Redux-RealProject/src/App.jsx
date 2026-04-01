275cd331b83626fdc234aac0aa208fa2e538a3e7
Musahaf Ali <musahafali43@gmail.com>
2026-04-01 10:46:00 +0500

    I setup folder structure and wrote the code of api for online data fetching

--- Redux-RealProject/src/App.jsx
+++ Redux-RealProject/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { fetchPhotos, fetchVideos } from './api/mediaApi';
 
 const App = () => {
   return (
     <div className='text-white h-screen w-full bg-gray-950'>
-      <h1 className='text-2xl'>Musahaf Bhai Kese Ho Aap</h1>
+      <button onClick={async ()=>{
+        const data =  await fetchPhotos('cat');
+        console.log(data.results);       
+        
+      }}>Get Data</button>
+      <button onClick={async ()=>{
+        const data =  await fetchVideos('cat');
+        console.log(data.videos);       
+        
+      }}>Get Videos</button>
     </div>
   );
 }
